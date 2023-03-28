a18b525d33dc8465071fd1e92d1e240fe6bf2199
Susu Ko <willy9966@gmail.com>
2023-03-28 17:11:11 +0800

    update-timer: add pomodoro

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -3,10 +3,13 @@ import { useState, useEffect } from 'react';
 import Header from './Header';
 import './Timer.css';
 
+const METHOD = {
+    '52/17': [52 * 60, 17 * 60],
+    pomodoro: [25 * 60, 5 * 60]
+}
 
-const restPeriod = 17 * 60;
-const workPeriod = 52 * 60;
 function Timer() {
+    const [workingMethod, setWorkingMethod] = useState('52/17');
     const [isRestTimeCounting, setIsRestTimeCounting] = useState(false);
     const [isWorkTimeCounting, setIsWorkTimeCounting] = useState(false);
 
@@ -21,7 +24,7 @@ function Timer() {
                 const remaining = (endTime - (new Date()).getTime()) / 1000;
                 if (remaining <= 0) {
                     setIsWorkTimeCounting(false);
-                    const endDate = (new Date().getTime()) + restPeriod * 1000;
+                    const endDate = (new Date().getTime()) + METHOD[workingMethod as keyof typeof METHOD][1] * 1000;
                     setEndTime(endDate);
                     setIsRestTimeCounting(true);
                 }
@@ -50,14 +53,19 @@ function Timer() {
         }
     }, [isRestTimeCounting]);
 
+
     return (
         <div className="Timer">
             <Header/>
+            <div className='timer-state'>
+                <div className='timer-option' aria-disabled={'52/17' !== workingMethod} onClick={() => {setWorkingMethod('52/17');}}>52/17</div>
+                <div className='timer-option' aria-disabled={'pomodoro' !== workingMethod} onClick={() => {setWorkingMethod('pomodoro');}}>Pomodoro</div>
+            </div>
             <h1 className='title'>{isRestTimeCounting ? 'Rest Time' : 'Work Time'}</h1>
             <p className='countdown'>{`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}</p>
             { endTime === 0 ? <></> : (<p className='end-time'>{(new Date(endTime)).toLocaleString('en-US')}</p>)}
             <button className='start' onClick={() => {
-                const endDate = (new Date().getTime()) + workPeriod * 1000;
+                const endDate = (new Date().getTime()) + METHOD[workingMethod as keyof typeof METHOD][0] * 1000;
                 setEndTime(endDate);
                 setIsWorkTimeCounting(true);
             }} disabled={isWorkTimeCounting || isRestTimeCounting}>Start</button>
