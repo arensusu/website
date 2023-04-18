7bdf77e6ed0e13a4d12a0dc8ae22616671cbc349
susuko <willy9966@gmail.com>
2023-04-18 22:38:56 +0800

    Update bookkeeping: searchform

--- src/Bookkeeping.tsx
+++ src/Bookkeeping.tsx
@@ -2,14 +2,14 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import Header from "./Header";
 
-const BASE_API = "https://192.168.0.16:8000/api"
+const BASE_API = "http://192.168.0.16:8000/api"
 
 const Bookkeeping = () => {
 
     const [apiData, setApiData] = useState([]);
     const [searchFormData, setSearchFormData] = useState<{ user: string; startDate: string; endDate: string; category: string[]}>({ user: '', startDate: '', endDate: '', category: []});
     const [addFormData, setAddFormData] = useState<{ user: string; date: string; category: string; cost: string}>({ user: '', date: '', category: '', cost: ''});
-    const [category, setCategory] = useState([])
+    const [category, setCategory] = useState([]);
 
     const handleSearchFormChange = (event: ChangeEvent<HTMLInputElement>) => {
         const attrName = event.target.name;
@@ -28,6 +28,16 @@ const Bookkeeping = () => {
         }
     };
 
+    const handleAddFormChange = (event: ChangeEvent<HTMLInputElement|HTMLSelectElement>) => {
+        const attrName = event.target.name;
+        const attrValue = event.target.value;
+        if (attrName === 'add-category') {
+            setAddFormData({ ...addFormData, category: attrValue});
+        } else {
+            setAddFormData({...addFormData, [attrName]: attrValue});
+        }
+    };
+
     const printDetails = (data: any[]) => {
         return data.map(item => {
             return (
@@ -43,8 +53,9 @@ const Bookkeeping = () => {
 
     useEffect(() => {
         fetch(`${BASE_API}/categories`)
-        .then(response => response.json())
-        .then(data => setCategory(data))
+            .then(response => response.json())
+            .then(data => setCategory(data))
+            .then(() => console.log('test'));
     }, []);
 
     const readData = (event: FormEvent<HTMLFormElement>) => {
@@ -52,34 +63,50 @@ const Bookkeeping = () => {
         const { category, ...others} = searchFormData;
         const queryApi = `${BASE_API}/details?${new URLSearchParams(others).toString()}`;
         fetch(queryApi)
-        .then(response => response.json())
-        .then(data => {
-            if (category.length !== 0) {
-                setApiData(data.filter((item: any) => category.includes(item.category.name)));
-            } else {
-                setApiData(data);
-            }
-        });
+            .then(response => response.json())
+            .then(data => {
+                if (category.length !== 0) {
+                    setApiData(data.filter((item: any) => category.includes(item.category.name)));
+                } else {
+                    setApiData(data);
+                }
+            });
         setSearchFormData({ user: '', startDate: '', endDate: '', category: []});
     };
 
     const writeData = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const body = {
+            user: { name: addFormData.user },
+            category: { name: addFormData.category },
+            cost: addFormData.cost,
+            date: addFormData.date
+        }
         fetch(`${BASE_API}/details`, {
-            method: 'POST', 
+            method: 'post', 
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(addFormData),
+            body: JSON.stringify(body),
         })
-        .then(response => response.json())
-        .catch(error => console.log(error));
+            .then(response => {
+                
+            });
+        setAddFormData({ user: '', date: '', cost: '', category: '' });
     };
 
-    const getCategory = (data: any[]) => {
+    const searchCategory = (data: any[]) => {
         return data.map(item => {
             return (
                 <label htmlFor={`search-${item.id}`} key={item.id}>
                     <input type='checkbox' name='search-category' id={`search-${item.id}`} value={item.name} checked={searchFormData.category.includes(item.name)} onChange={handleSearchFormChange} />{item.name}
                 </label>
+            )
+        })
+    }
+
+    const addCategory = (data: any[]) => {
+        return data.map(item => {
+            return (
+                <option value={item.name} key={item.id}>{item.name}</option>
             )
         })
     }
@@ -99,7 +126,7 @@ const Bookkeeping = () => {
                         <input type='date' id='search-end-date' name='endDate' value={searchFormData.endDate} onChange={handleSearchFormChange} />
                         <div>
                             <span>Category: </span>
-                            {getCategory(category)}
+                            {searchCategory(category)}
                         </div>
                         <input type='submit' value='Search' />
                     </form>
@@ -108,11 +135,13 @@ const Bookkeeping = () => {
                 <div className='write'>
                     <h2>Add a new detail</h2>
                     <form id='add-form' onSubmit={writeData}>
-                        <label htmlFor='add-username'>Username: </label><input type='text' id='add-username' />
-                        <label htmlFor='add-date'>Date: </label><input type='date' id='add-date' />
+                        <label htmlFor='add-username'>Username: </label><input type='text' id='add-username' name='user' value={addFormData.user} onChange={handleAddFormChange} />
+                        <label htmlFor='add-date'>Date: </label><input type='date' id='add-date' name='date' value={addFormData.date} onChange={handleAddFormChange} />
+                        <label htmlFor='add-cost'>Cost: </label><input type='text' id='add-cost' name='cost' value={addFormData.cost} onChange={handleAddFormChange} />
                         <label htmlFor='add-category'>Category: </label>
-                        <select name='add-category' id='add-category'>
-                            <option value='all'>All</option>
+                        <select name='add-category' id='add-category' value={addFormData.category} onChange={handleAddFormChange}>
+                            <option value=''>Please select</option>
+                            {addCategory(category)}
                         </select>
                         <input type='submit' value='Add' />
                     </form>
